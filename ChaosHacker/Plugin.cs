df8f8b5e156d49390b4133b56a6df649801c08a2
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2021-01-11 23:25:25 +0100

    Add files via upload

---
+++ ChaosHacker/Plugin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+using PlayerEvent = Exiled.Events.Handlers.Player;
+using ServerEvent = Exiled.Events.Handlers.Server;
+using MapEvent = Exiled.Events.Handlers.Map;
+
+namespace ChaosHacker
+{
+    public class ChaosHacker : Plugin<Config>
+    {
+        private static readonly Lazy<ChaosHacker> LazyInstance = new Lazy<ChaosHacker>(() => new ChaosHacker());
+        public static ChaosHacker Instance => LazyInstance.Value;
+
+        public override PluginPriority Priority => PluginPriority.Medium;
+
+        public override string Author => "Michal78900";
+        public override Version Version => new Version(1, 0, 0);
+
+        private ChaosHacker() { }
+
+        private Handler handler;
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            handler = new Handler();
+
+            ServerEvent.RespawningTeam += handler.OnTeamRespawn;
+
+            PlayerEvent.Left += handler.OnPlayerLeft;
+            PlayerEvent.Died += handler.OnPlayerDied;
+            PlayerEvent.ChangingRole += handler.OnPlayerRoleChange;
+
+
+
+
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            ServerEvent.RespawningTeam -= handler.OnTeamRespawn;
+
+            PlayerEvent.Left -= handler.OnPlayerLeft;
+            PlayerEvent.Died -= handler.OnPlayerDied;
+            PlayerEvent.ChangingRole -= handler.OnPlayerRoleChange;
+
+            handler = null;
+        }
+    }
+}
---
+++ ChaosHacker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("ChaosHacker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChaosHacker")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("35034190-5cfc-470a-8f57-f414d7b44da9")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
